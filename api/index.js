52a57259631ffad504f366072d871120586bef44
ashraf hamdy <ashrafkmh@gmail.com>
2024-03-22 00:37:13 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-import exp from "constants";
+import mongoose from "mongoose";
 import  Express  from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB!!!');
+}).catch(err => {  
+    console.log('Error: ', err.message);
+});
 const app = Express();
 app.listen(3000, () => {
-    console.log('App listening on port 3000!!');
+    console.log('App listening on port 3000!!!');
 });
