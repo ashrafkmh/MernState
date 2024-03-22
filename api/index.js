115e3c69f7238dbf32a98973fa847cfdf4624980
ashraf hamdy <ashrafkmh@gmail.com>
2024-03-22 22:01:43 +0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
-import mongoose from "mongoose";
-import  Express  from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import mongoose from 'mongoose';
+import  Express  from 'express';
+import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js"
+
+
+
+dotenv.config(); 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB!!!');
 }).catch(err => {  
     console.log('Error: ', err.message);
 });
 const app = Express();
-app.listen(3000, () => {
-    console.log('App listening on port 3000!!!');
+
+
+app.listen(5000, () => {
+    console.log('App listening on port 5000!!!');
 });
+app.use('/api/user', userRouter);
---
+++ api/routes/user.route.js
@@ -0,0 +1,10 @@
+
+import Express from 'express';
+import { test } from '../controllers/user.controller.js';
+
+
+
+const userRouter = Express.Router();
+userRouter.get('/test', test);
+
+export default userRouter;  
